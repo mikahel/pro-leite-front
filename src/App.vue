cc54a2b25cd6f5159c68e84df20f7142a3dfbbe4
Ismael <iraj@cin.ufpe.br>
2021-03-26 22:14:34 -0300

    Ajusta cores do background da aplicação

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(85,9,121,1) 52%, rgba(0,212,255,1) 100%); 
 }
 
 #nav {
@@ -26,5 +28,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+html {
+  background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(85,9,121,1) 52%, rgba(0,212,255,1) 100%); 
 }
 </style>
