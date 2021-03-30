5bdd6ce15afc376635eb49203c4f2e835c720c7d
Carolina Melo <caroolpmelo@gmail.com>
2021-03-29 22:32:20 -0300

    :construction: WIP Some adjustments in login page
    Signed-off-by: Carolina Melo <caroolpmelo@gmail.com>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../public/img/proLeiteLogo.png" />
-    
+    <form class="buttons-container" action="/action_page.php">
+      <input placeholder="E-mail" type="text" id="femail" name="femail">
+      <input placeholder="Senha" type="password" id="fpassword" name="fpassword">
+      <input type="submit" value="Submit">
+    </form>
     <Buttom msg="Entrar" />
   </div>
 </template>
@@ -17,3 +21,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  .buttons-container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0;
+    text-align-last: center;
+    width: 300px;
+  }
+  input {
+    border-radius: 10px;
+  }
+</style>
